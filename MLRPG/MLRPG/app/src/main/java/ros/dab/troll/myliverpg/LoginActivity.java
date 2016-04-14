a4d7e3cb624433ba87a716ff7884908927da1bd8
Kacper Roszczyna <kacper.ros94@gmail.com>
2016-04-14 21:07:28 +0200

    initial android commit

---
+++ MLRPG/MLRPG/app/src/main/java/ros/dab/troll/myliverpg/LoginActivity.java
@@ -0,0 +1,45 @@
+package ros.dab.troll.myliverpg;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initOnClickListeners();
+    }
+
+    private void initOnClickListeners() {
+        findViewById(R.id.login_button).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if(viewId == R.id.login_button){
+            EditText loginEditText = (EditText) findViewById(R.id.login_edittext);
+            String loginText = loginEditText.getText().toString();
+
+            EditText passwordEditText = (EditText) findViewById(R.id.password_edittext);
+            String password = passwordEditText.getText().toString();
+
+            if(loginText.equals("admin") && password.equals("password")){
+                Toast.makeText(this, "Hi logged in user", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Inappropiate identification", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
